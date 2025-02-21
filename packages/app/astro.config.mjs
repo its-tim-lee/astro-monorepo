3072dfd7b154e115c02bf0518f5265a8f0c20369
timlee <hungtington.dev@gmail.com>
2025-02-20 21:06:22 -0500

    Initial monorepo setup

---
+++ packages/app/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [react()]
+});
---
+++ packages/ds/src/components/Button.tsx
@@ -0,0 +1,4 @@
+export default function Button() {
+  return <button className="bg-blue-500 text-white px-4 py-2 rounded">Click me</button>;
+}
+
